873ade65cff907a8927de085d38a0bba12373026
aninha20 <101808972+aninha20@users.noreply.github.com>
2022-08-29 23:01:24 -0300

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+/*!
+* Start Bootstrap - One Page Wonder v6.0.5 (https://startbootstrap.com/theme/one-page-wonder)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-one-page-wonder/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+alert("Cuidado! Você está correndo o risco de ser invadido por um mundo de doçura ;)")
